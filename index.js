5d6bc60b968275f463090c87128a21e1216459e2
cathy473 <njericathy473@gmail.com>
2023-06-23 01:07:32 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(name) {
+  for (let i = 0; i < name.length; i++) {
+    console.log("Thank you, ${name[i]}, for the wonderful suprise gift!");
+    debugger;
+  }
+
+  return name;
+}
+
+writeCards(name);
+
+function countdown() {
+    let countdown = 11;
+    while (countdown < 0) {
+        console.log(countdown--);
+      }
+      return countdown;
+}
+
+
+
+
